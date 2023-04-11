8b501e24ba08df400d042dc80587c4acbffbee40
Maxim Skory <devisrush@gmail.com>
2023-04-11 19:54:03 +0300

    final commit

--- src/main/java/com/shpp/level0/practice/App.java
+++ src/main/java/com/shpp/level0/practice/App.java
@@ -3,9 +3,6 @@ package com.shpp.level0.practice;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -13,12 +10,14 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import java.util.Properties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class App {
     public static final String PROPERTIES_FILE_NAME = "app.properties";
     public static final String USERNAME_KEY = "username";
-    private static final String SYSTEM_OPTION_KEY = "format";
-    private static final String XML_OUTPUT_FORMAT = "xml";
+    private static final String SYSTEM_OPTION_FILE_FORMAT_KEY = "format";
+    private static final String XML_FORMAT_VALUE = "xml";
     private static final String GREETING_LINE = "Привіт";
     private static final String EXCLAMATION_POINT = "!";
     private static final Logger logger = LoggerFactory.getLogger(App.class);
@@ -28,18 +27,33 @@ public class App {
     }
 
     void run() {
-        Properties properties = getJarProperties();
-        String externalPropertiesPath = getPathToExternalProperties();
-        properties.putAll(getExternalProperties(externalPropertiesPath));
-        String name = properties.getProperty(USERNAME_KEY);
-        String message = GREETING_LINE + " " + name + EXCLAMATION_POINT;
-        Message greetingMessage = new Message(message);
-        if (isFileTypeXml(SYSTEM_OPTION_KEY)) {
-            message = transformMessage(greetingMessage, new XmlMapper());
-        } else {
-            message = transformMessage(greetingMessage, new ObjectMapper());
-        }
+        Properties currentProperties = mergeProperties(getJarProperties(), getExternalProperties());
+        Message greetingMessage = new Message(getMessage(currentProperties, USERNAME_KEY));
+        String message = getFormattedMessage(greetingMessage);
+        logger.trace("Printing message");
         System.out.println(message);
+    }
+
+    private String getFormattedMessage(Message greetingMessage) {
+        logger.trace("Formatting message to a specific type");
+        return isFileTypeXml(SYSTEM_OPTION_FILE_FORMAT_KEY)
+                ? transformMessage(greetingMessage, new XmlMapper()) :
+                transformMessage(greetingMessage, new ObjectMapper());
+
+    }
+
+    private String getMessage(Properties currentProperties, String usernameKey) {
+        logger.trace("Processing new message");
+        String name = currentProperties.getProperty(usernameKey);
+        return GREETING_LINE + " " + name + EXCLAMATION_POINT;
+    }
+
+    private Properties mergeProperties(Properties internalProperties, Properties externalProperties) {
+        if (!externalProperties.isEmpty()) {
+            logger.info("Merging internal and external property files");
+            internalProperties.putAll(externalProperties);
+        }
+        return internalProperties;
     }
 
     private String getPathToExternalProperties() {
@@ -49,29 +63,32 @@ public class App {
 
     private boolean isFileTypeXml(String key) {
         String property = System.getProperty(key);
-        return property != null && property.equals(XML_OUTPUT_FORMAT);
+        return property != null && property.equals(XML_FORMAT_VALUE);
     }
 
     private Properties getJarProperties() {
+        logger.info("Reading internal properties file");
         try {
             Properties properties = new Properties();
-            properties.load(new InputStreamReader(Objects.requireNonNull(getClass().
-                    getClassLoader().getResourceAsStream(PROPERTIES_FILE_NAME))));
+            properties.load(new InputStreamReader(Objects.requireNonNull(getClass()
+                    .getClassLoader().getResourceAsStream(PROPERTIES_FILE_NAME))));
             return properties;
         } catch (IOException e) {
             throw new RuntimeException("Can't read " + PROPERTIES_FILE_NAME + " from jar.");
         }
     }
 
-    private Properties getExternalProperties(String path) {
+    private Properties getExternalProperties() {
+        logger.info("Reading external properties file");
+        String path = getPathToExternalProperties();
         Properties properties = new Properties();
         try {
             properties.load(new FileReader(path + "/" + PROPERTIES_FILE_NAME, StandardCharsets.UTF_8));
             return properties;
         } catch (IOException e) {
-            logger.info("The external properties file " + path + PROPERTIES_FILE_NAME +
-                    " not found" + System.lineSeparator() +
-                    "The program will be executed with default properties file");
+            logger.warn("The external properties file " + path + PROPERTIES_FILE_NAME
+                    + " not found" + System.lineSeparator()
+                    + "The program will be executed with default properties file");
         }
         return properties;
     }
@@ -84,11 +101,16 @@ public class App {
         }
     }
 
-    static class Message {
-        public final String message;
+    private static class Message {
+        private final String message;
 
         public Message(String message) {
+            logger.trace("Creating a new Message instance");
             this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
         }
     }
 }
